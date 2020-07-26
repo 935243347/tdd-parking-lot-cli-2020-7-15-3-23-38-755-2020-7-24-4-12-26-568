619ba9b680a637b4bfa2afc531dc6cbc41fdf7d9
Max Miao <miaoma2@oocl.com>
2020-07-26 19:58:56 +0800

    [Parking Lot][Max] test: test should_print_not_enough_position_when_parkCar_given_1_super_smart_parking_boy_1_car_park_in_full_capacity_parking_lot

--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -166,4 +166,22 @@ public class SuperSmartParkingBoyTest {
         Assertions.assertNull(resultCar);
         Assertions.assertEquals("Please provide your parking ticket.", outContent.toString());
     }
+
+    @Test
+    void should_print_not_enough_position_when_parkCar_given_1_super_smart_parking_boy_1_car_park_in_full_capacity_parking_lot() {
+        //Given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot());
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+        for (int i = 0; i < 10; i++) {
+            Car car = new Car("100" + i);
+            superSmartParkingBoy.parkCar(car);
+        }
+        //When
+        Car car = new Car("2001");
+        Ticket ticket = superSmartParkingBoy.parkCar(car);
+        //Then
+        Assertions.assertNull(ticket);
+        Assertions.assertEquals("Not enough position.", outContent.toString());
+    }
 }
