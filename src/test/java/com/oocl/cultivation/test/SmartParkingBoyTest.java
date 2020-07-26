3dd5b9325eacf8c935ccb1d6f871127d15e14e16
Max Miao <miaoma2@oocl.com>
2020-07-26 19:02:24 +0800

    [Parking Lot][Max] test: test should_return_2_ticket_when_parkCar_given_1_smart_parking_boy_2_car_1_parking_lot

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SmartParkingBoyTest {
     @Test
     void should_return_1_ticket_when_parkCar_given_1_smart_parking_boy_1_car_1_parking_lot() {
@@ -34,5 +36,22 @@ public class SmartParkingBoyTest {
         Car resultCar = smartParkingBoy.fatchCar(ticket);
         //Then
         Assertions.assertNotNull(resultCar);
+    }
+
+    @Test
+    void should_return_2_ticket_when_parkCar_given_1_smart_parking_boy_2_car_1_parking_lot() {
+        //Given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot());
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car1 = new Car("1001");
+        Car car2 = new Car("1002");
+        List<Car> carList = new ArrayList<>();
+        carList.add(car1);
+        carList.add(car2);
+        //When
+        List<Ticket> parkTickets = smartParkingBoy.parkCar(carList);
+        //Then
+        assertEquals(2, parkTickets.size());
     }
 }
